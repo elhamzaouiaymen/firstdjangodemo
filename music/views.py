e35c7449c1e806a038d0fb371c9744fde77af162
aymen hamzaoui <aymen hamzaoui>
2016-12-06 19:27:48 +0100

    working with templates UPDATED ...

--- music/views.py
+++ music/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from .models import  Album
+from django.template import loader
+
+
+
 def index(request):
-    return HttpResponse("<h1> this is the music your are looking for ...</h1>")
+    all_Albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_Albums
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details of "+str(album_id)+"</h2>")
